147d94ba0159b8e966c1ae30db3d81547e66574b
54771059 <darkvalvet@protonmail.com>
2025-12-29 19:02:50 +0200

    fix: adds auth checking before todo page

--- src/presentation/pages/router.ts
+++ src/presentation/pages/router.ts
@@ -3,12 +3,13 @@ import { showHomePage } from "./controllers/home";
 import { showLoginPage } from "./controllers/login";
 import { showTodoPage } from "./controllers/todo";
 import { showRegisterPage } from "./controllers/register";
+import { requireAuth } from "../middleware/session";
 
 const pagesRouter = Router();
 
 pagesRouter.get("/", showHomePage);
 pagesRouter.get("/login", showLoginPage);
 pagesRouter.get("/register", showRegisterPage);
-pagesRouter.get("/todo", showTodoPage);
+pagesRouter.get("/todo", requireAuth ,showTodoPage);
 
 export default pagesRouter;
